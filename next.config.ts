7358fc4a82224ffc5558f208d83aca87eecc9ee0
Abdelaziz Ebrahim <abdelazizebrahim5980@gmail.com>
2024-12-04 21:07:10 +0300

    Product listing completed with product card

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        pathname: "/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/a/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
